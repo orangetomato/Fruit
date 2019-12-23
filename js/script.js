0c68d0581e7eb6639823706159fc6beec480f86d
orangetomato <itaintme@mail.ru>
2019-12-23 18:31:26 +0300

    Анимация 1

--- js/script.js
+++ js/script.js
@@ -9,37 +9,68 @@ let images = ["orange.jpg", "cherry.jpg", "apple.jpg"];
 let getRandomImage = function () {
     return Math.floor(Math.random() * images.length);
 };
-let currentImageIndex = getRandomImage()
+let currentImageIndex = getRandomImage();
 
-let body = document.querySelector("body");
-body.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
+let background = document.querySelector(".background");
+let container = document.querySelector(".container");
+container.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
+
+let pressedLeft = 0;
+let pressedRight = 0;
 
 let nextImage = function () {
+    background.classList.remove("overlay-left");
+    background.classList.remove("background-left");
+    if (pressedRight % 2 !== 0) {
+        background.classList.remove("overlay-right");
+        background.classList.add("background-right");
+    } else {
+        background.classList.add("overlay-right");
+        background.classList.remove("background-right");
+    };
+
     let nextImageIndex = currentImageIndex + 1;
     if (nextImageIndex >= images.length) {
         nextImageIndex = 0;
     };
     currentImageIndex = nextImageIndex;
-    body.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
+    background.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
 };
 
 let previousImage = function () {
+    background.classList.remove("overlay-right");
+    background.classList.remove("background-right");
+    if (pressedLeft % 2 !== 0) {
+        background.classList.remove("overlay-left");
+        background.classList.add("background-left");
+    } else {     
+        background.classList.add("overlay-left");
+        background.classList.remove("background-left");
+    };
+
     let previousImageIndex = currentImageIndex - 1;
     if (previousImageIndex < 0) {
         previousImageIndex = images.length - 1;
     };
     currentImageIndex = previousImageIndex;
-    body.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
+    background.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
 };
 
 window.addEventListener("keydown", function(evt) {
+    background.style.display = "block";
+    if (background.style.backgroundImage) {
+        container.style.backgroundImage = background.style.backgroundImage;
+    };
+
     if (evt.keyCode === RIGHT_KEYCODE) {
         evt.preventDefault();
+        pressedRight +=1;
         nextImage();
     };
 
     if (evt.keyCode === LEFT_KEYCODE) {
         evt.preventDefault();
+        pressedLeft +=1;
         previousImage();
     };
 });
