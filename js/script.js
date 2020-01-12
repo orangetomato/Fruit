2c49192d82952fc077713ef9c8a06be659bdc83e
orangetomato <itaintme@mail.ru>
2020-01-12 10:58:57 +0300

    Сократил код

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const RIGHT_KEYCODE = 39;
 let folder = "img/";
 let images = ["orange.jpg", "cherry.jpg", "apple.jpg"];
 
-let getRandomImage = function () {
+let getRandomImage = function() {
     return Math.floor(Math.random() * images.length);
 };
 let currentImageIndex = getRandomImage();
@@ -14,69 +14,51 @@ let currentImageIndex = getRandomImage();
 let background = document.querySelector(".background");
 let overlay = document.querySelector(".overlay");
 overlay.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
-overlay.classList.add("bounce");
 
-let pressedLeft = 0;
-let pressedRight = 0;
-
-let nextImage = function () {
+let nextImage = function() {
     let nextImageIndex = currentImageIndex + 1;
     if (nextImageIndex >= images.length) {
         nextImageIndex = 0;
     };
     currentImageIndex = nextImageIndex;
 
-    overlay.classList.remove("overlay-left");
-    background.classList.remove("background-left");
-    if (pressedRight % 2 !== 0) {
-        overlay.classList.remove("overlay-right");
-        background.classList.add("background-right");
-        background.style.display = "block";
-        background.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
-    } else {
-        overlay.classList.add("overlay-right");
-        background.classList.remove("background-right");
-        overlay.style.display = "block";
-        overlay.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
-    };
+    overlay.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
+    background.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
 };
 
-let previousImage = function () {
+let previousImage = function() {
     let previousImageIndex = currentImageIndex - 1;
     if (previousImageIndex < 0) {
         previousImageIndex = images.length - 1;
     };
     currentImageIndex = previousImageIndex;
 
-    overlay.classList.remove("overlay-right");
-    background.classList.remove("background-right");
-    if (pressedLeft % 2 !== 0) {
-        overlay.classList.remove("overlay-left");
-        background.classList.add("background-left");
-        background.style.display = "block";    
-        background.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
-    } else {
-        overlay.classList.add("overlay-left");
-        background.classList.remove("background-left");
-        overlay.style.display = "block";
-        overlay.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
-    };
+    overlay.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
+    background.style.backgroundImage = "url(" + folder + images[currentImageIndex] + ")";
 };
 
 window.addEventListener("keydown", function(evt) {
     overlay.classList.remove("bounce");
-    overlay.style.display = "none";
-    background.style.display = "none";
+
+    if (evt.keyCode === RIGHT_KEYCODE || evt.keyCode === LEFT_KEYCODE) {
+        evt.preventDefault();
+        overlay.classList.toggle("hide");
+        background.classList.toggle("hide");
+    };
 
     if (evt.keyCode === RIGHT_KEYCODE) {
-        evt.preventDefault();
-        pressedRight +=1;
+        background.classList.remove("left");
+        overlay.classList.remove("left");
+        background.classList.add("right");
+        overlay.classList.add("right");
         nextImage();
     };
     
     if (evt.keyCode === LEFT_KEYCODE) {
-        evt.preventDefault();
-        pressedLeft +=1;
+        background.classList.remove("right");
+        overlay.classList.remove("right");
+        background.classList.add("left");
+        overlay.classList.add("left");
         previousImage();
     };
 });
